cc29f2e6c6718130c1ab950f12595690597207fe
ericali1125 <ericamax1998@gmail.com>
2024-03-05 20:38:32 +1100

    functions practice

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+
+
+/*
+const returnFirstTwoDrivers = (drivers) => [drivers[0],drivers[1]];
+
+const returnLastTwoDrivers = function (drivers) {
+    const numberOfDrivers = drivers.length;
+    return [drivers[numberOfDrivers - 2], drivers[numberOfDrivers -1]];
+};
+
+const createFareM
+
+*/
+
+// returnFirstTwoDrivers function
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // returnLastTwoDrivers function
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // selectingDrivers array
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // createFareMultiplier higher-order function
+  const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+  
+  // fareDoubler function
+  const fareDoubler = createFareMultiplier(2);
+  
+  // fareTripler function
+  const fareTripler = createFareMultiplier(3);
+  
+  // selectDifferentDrivers function
+  const selectDifferentDrivers = function(drivers, selectFunction) {
+    return selectFunction(drivers);
+  };
+  
